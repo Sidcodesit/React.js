93fbabe6d705322aa30dd269681c585dff4d829f
Siddharth Parmar <siddharthparmar.tech@gmail.com>
2025-10-14 23:56:40 +0530

    added

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,13 @@
 
 import NewFnc from "./new"
 function App() {
-
+  const username = "siddharth"
 
   return (
+    <>
     <NewFnc/>
+    <h1>hello {username}</h1>
+    </>
   )
 }
 
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,50 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import NewFnc from './new.jsx'
+
+function NewApp(){
+  return(
+    <div>
+      <h1>hello newApp</h1>
+    </div>
+  )
+}
+
+const element = (
+  <a href= "https://google.com" target='_blank'>Visit google</a>
+)
+
+
+// const Elements = {
+//     type: "div",
+//     props:{
+//         className:"main-box",
+//         id:"box"
+//     },
+//     Children: "hello my name is sid",
+// }
+//                             WE CANNOT DEFINE LIKE THIS BECAUSE RENDER NEEDS A PROPER SYNTAX 
+
+// React.createElement()
+// in this we have to pass paramter in an order
+// 1. type of tag
+// 2. HTML attributes 
+// 3. inner HTML 
+
+const userName = "sidcodesit"
+
+const oneMoreElement = React.createElement(
+  'a',
+  {href: "https://google.com",target: '_blank'},
+  "click here to visit it",
+  // we can set any value also like {username} type
+  userName
+)
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+//  <NewApp/>
+// element
+// oneMoreElement
+<App/>
 )
