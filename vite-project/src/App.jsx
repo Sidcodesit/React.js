4ac5976c902463a07f523b768fa81820aa0b67bf
Siddharth Parmar <siddharthparmar.tech@gmail.com>
2025-10-19 00:34:13 +0530

    added

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [value, increaseValue] = useState("siddharth")
+
+
+  function clicked(){
+    value = "sid"
+    increaseValue(value)
+  }
+
+  return (
+    <>
+    <h1>Counter value {value}</h1>
+    <h2>new counter value {value}</h2>
+    <button onClick={clicked}>increase counter value</button>
+    </>
+  )
+}
+
+export default App
