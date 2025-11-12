bd24fb9cfc8c5a3ed3c37833d8487ac7d4f99374
Siddharth Parmar <siddharthparmar.tech@gmail.com>
2025-11-12 21:15:18 +0530

    added

--- BackgroundChanger/src/App.jsx
+++ BackgroundChanger/src/App.jsx
@@ -1,20 +1,10 @@
 import React from 'react'
-import { useState } from 'react'
+import Color from '../components/Color'
 
 const App = () => {
-  const [color,setColor] = useState("white")
+
   return (
-    <div className='h-screen w-full duration-200' style={{backgroundColor: color}}>
-      <div className=' flex flex-wrap fixed left-10 top-10 justify-center'>
-        <div className='flex flex-wrap flex-col justify-center gap-5 shadow-lg bg-white rounded-2xl px-3 py-2'>
-          <button className='bg-red-500  outline-none px-2 py-2 rounded-2xl text-white shadow-lg' onClick={()=>setColor("red")}>Red</button>
-          <button className='bg-blue-500 outline-none px-2 py-2 rounded-2xl text-white shadow-lg'onClick={()=>setColor("blue")}>Blue</button>
-          <button className='bg-green-500 outline-none px-2 py-2 rounded-2xl text-white shadow-lg' onClick={()=>setColor("green")}>green</button>
-          <button className='bg-pink-500 outline-none px-2 py-2 rounded-2xl text-white shadow-lg' onClick={()=>setColor("pink")}>pink</button>
-          <button className='bg-yellow-400 outline-none px-2 py-2 rounded-2xl text-white shadow-lg' onClick={()=>setColor("yellow")}>yellow</button>
-        </div>
-      </div>
-    </div>
+    <Color/>
   )
 }
 
